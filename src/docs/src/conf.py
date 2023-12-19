6ba306a9e05e39727d7f3b73773b716572bc688f
Jack <jack20220723@gmail.com>
2023-12-19 15:49:35 +0800

    Publish documentation to GitHub Pages

--- src/docs/src/conf.py
+++ src/docs/src/conf.py
@@ -45,6 +45,9 @@ primary_domain = "http"
 
 pygments_style = "sphinx"
 
+
+html_baseurl = "https://QubitPi.github.io/couchdb/"
+
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
